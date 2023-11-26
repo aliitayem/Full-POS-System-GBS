1bb677034bb36285ac6f8050c921bdc6c035fbf6
aliitayem <111657014+aliitayem@users.noreply.github.com>
2023-11-26 13:51:45 -0600

    Update Space_Planner.py

--- Space_Planner.py
+++ Space_Planner.py
@@ -3,102 +3,61 @@ import tkinter as tk
 class WallGUI:
     def __init__(self):
         self.root = tk.Tk()
-        self.canvas = tk.Canvas(self.root, width=1500, height=1000)
+        self.root.title("Beauty Supply Store Organizer")
+
+        # Create labels and entry boxes for product details
+        product_frame = tk.Frame(self.root)
+        product_frame.pack()
+
+        tk.Label(product_frame, text="Product Name:").grid(row=0, column=0)
+        self.name_entry = tk.Entry(product_frame)
+        self.name_entry.grid(row=0, column=1)
+
+        tk.Label(product_frame, text="Brand:").grid(row=1, column=0)
+        self.brand_entry = tk.Entry(product_frame)
+        self.brand_entry.grid(row=1, column=1)
+
+        tk.Label(product_frame, text="Color:").grid(row=2, column=0)
+        self.color_entry = tk.Entry(product_frame)
+        self.color_entry.grid(row=2, column=1)
+
+        tk.Label(product_frame, text="Width:").grid(row=3, column=0)
+        self.width_entry = tk.Entry(product_frame)
+        self.width_entry.grid(row=3, column=1)
+
+        tk.Label(product_frame, text="Height:").grid(row=4, column=0)
+        self.height_entry = tk.Entry(product_frame)
+        self.height_entry.grid(row=4, column=1)
+
+        # Create buttons to add product and obstruction
+        action_frame = tk.Frame(self.root)
+        action_frame.pack()
+
+        add_product_button = tk.Button(action_frame, text="Add Product", command=self.add_product)
+        add_product_button.grid(row=0, column=0)
+
+        add_obstruction_button = tk.Button(action_frame, text="Add Obstruction", command=self.add_obstruction)
+        add_obstruction_button.grid(row=0, column=1)
+
+        # Create canvas for wall representation
+        self.canvas = tk.Canvas(self.root, width=800, height=600, bg="white")
         self.canvas.pack()
 
-        self.products = []
-        self.obstructions = []
+    def add_product(self):
+        name = self.name_entry.get()
+        brand = self.brand_entry.get()
+        color = self.color_entry.get()
+        width = int(self.width_entry.get())
+        height = int(self.height_entry.get())
 
-        self.create_wall()
+        # Add product logic goes here
 
-        # Create entry boxes and buttons for product details, wall dimensions, and obstructions
-
-    def create_wall(self):
-        # Generate the 2D wall representation on the canvas based on user input
-        # Example code for drawing horizontal slits
-        slit_height = 3
-        slit_gap = 3
-        wall_width = 0  # Placeholders, update with actual values
-        wall_height = 0  # Placeholders, update with actual values
-
-        for y in range(0, wall_height, slit_height + slit_gap):
-            self.canvas.create_line(0, y, wall_width, y, fill="black")
+    def add_obstruction(self):
+        # Add obstruction logic goes here
+        pass
 
     def run(self):
         self.root.mainloop()
-
-if __name__ == "__main__":
-    app = WallGUI()
-    app.run()
-
-
-
-
-
-
-
-class WallGUI:
-    # ...
-
-    def add_product(self):
-        # Create a new Product instance with the entered details
-        name = "Product Name"  # Placeholder, replace with actual value from entry box
-        brand = "Product Brand"  # Placeholder, replace with actual value from entry box
-        color = "Product Color"  # Placeholder, replace with actual value from entry box
-        width = 0  # Placeholder, replace with actual value from entry box
-        height = 0  # Placeholder, replace with actual value from entry box
-
-        product = Product(name, brand, color, width, height)
-        self.products.append(product)
-
-        # Display the product frame on the drag and drop section
-        product_frame = tk.Frame(self.root, width=width, height=height, bg="white")  # Customize as needed
-        product_frame.bind("<ButtonPress-1>", self.start_drag)  # Bind mouse button press event for dragging
-        # Additional code for displaying the product frame
-
-        # Store the product details in the MySQL table
-        # Example code for MySQL database insertion:
-        # db.insert_product(name, brand, color, width, height)
-
-    # ...
-
-    def start_drag(self, event):
-        # Handle the start of product frame dragging
-        # Example code for capturing initial mouse coordinates:
-        # self.drag_data = {'x': event.x, 'y': event.y, 'frame': event.widget}
-
-    def drag_product(self, event):
-        # Handle the dragging of product frames within the canvas
-        # Example code for moving the dragged frame:
-        # delta_x = event.x - self.drag_data['x']
-        # delta_y = event.y - self.drag_data['y']
-        # self.canvas.move(self.drag_data['frame'], delta_x, delta_y)
-
-    # ...
-
-
-
-
-
-
-
-
-class WallGUI:
-    # ...
-
-    def add_obstruction(self):
-        # Create an obstruction object with the entered dimensions
-        width = 0  # Placeholder, replace with actual value from entry box
-        height = 0  # Placeholder, replace with actual value from entry box
-
-        obstruction = {'width': width, 'height': height}
-        self.obstructions.append(obstruction)
-
-        # Update the 2D wall representation to incorporate the obstruction
-        # Example code for drawing an obstruction rectangle on the canvas:
-        # self.canvas.create_rectangle(x1, y1, x2, y2, fill="black")
-
-    # ...
 
 if __name__ == "__main__":
     app = WallGUI()
