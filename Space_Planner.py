962018f61f51d9feef12b1ce64ae28a007999aeb
aliitayem <111657014+aliitayem@users.noreply.github.com>
2023-11-26 16:03:53 -0600

    Update Space_Planner.py

--- Space_Planner.py
+++ Space_Planner.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 class WallGUI:
     def __init__(self):
@@ -8,53 +9,53 @@ class WallGUI:
         self.conversion_rate = 20  # Conversion rate from inches to pixels
 
         # Create frame for wall dimensions
-        dimensions_frame = tk.Frame(self.root)
-        dimensions_frame.pack()
+        dimensions_frame = ttk.LabelFrame(self.root, text="Wall Dimensions")
+        dimensions_frame.pack(padx=10, pady=10)
 
         tk.Label(dimensions_frame, text="Wall Width:").grid(row=0, column=0)
         self.width_entry = tk.Entry(dimensions_frame)
-        self.width_entry.grid(row=0, column=1)
+        self.width_entry.grid(row=0, column=1, padx=5)
 
-        tk.Label(dimensions_frame, text="Wall Height:").grid(row=1, column=0)
+        tk.Label(dimensions_frame, text="Wall Height:").grid(row=0, column=2)
         self.height_entry = tk.Entry(dimensions_frame)
-        self.height_entry.grid(row=1, column=1)
+        self.height_entry.grid(row=0, column=3, padx=5)
 
         apply_button = tk.Button(dimensions_frame, text="Apply", command=self.update_wall)
-        apply_button.grid(row=2, columnspan=2)
+        apply_button.grid(row=0, column=4, padx=5)
 
-        # Create labels and entry boxes for product details
-        product_frame = tk.Frame(self.root)
-        product_frame.pack()
+        # Create Label Frame for product details
+        product_frame = ttk.LabelFrame(self.root, text="Product")
+        product_frame.pack(padx=10, pady=10)
 
         tk.Label(product_frame, text="Product Name:").grid(row=0, column=0)
         self.name_entry = tk.Entry(product_frame)
-        self.name_entry.grid(row=0, column=1)
+        self.name_entry.grid(row=0, column=1, padx=5)
 
-        tk.Label(product_frame, text="Brand:").grid(row=1, column=0)
+        tk.Label(product_frame, text="Brand:").grid(row=0, column=2)
         self.brand_entry = tk.Entry(product_frame)
-        self.brand_entry.grid(row=1, column=1)
+        self.brand_entry.grid(row=0, column=3, padx=5)
 
-        tk.Label(product_frame, text="Color:").grid(row=2, column=0)
+        tk.Label(product_frame, text="Color:").grid(row=1, column=0)
         self.color_entry = tk.Entry(product_frame)
-        self.color_entry.grid(row=2, column=1)
+        self.color_entry.grid(row=1, column=1, padx=5)
 
-        tk.Label(product_frame, text="Width:").grid(row=3, column=0)
+        tk.Label(product_frame, text="Width:").grid(row=1, column=2)
         self.prod_width_entry = tk.Entry(product_frame)
-        self.prod_width_entry.grid(row=3, column=1)
+        self.prod_width_entry.grid(row=1, column=3, padx=5)
 
-        tk.Label(product_frame, text="Height:").grid(row=4, column=0)
+        tk.Label(product_frame, text="Height:").grid(row=1, column=4)
         self.prod_height_entry = tk.Entry(product_frame)
-        self.prod_height_entry.grid(row=4, column=1)
+        self.prod_height_entry.grid(row=1, column=5, padx=5)
 
         # Create buttons to add product and obstruction
-        action_frame = tk.Frame(self.root)
-        action_frame.pack()
+        action_frame = ttk.Frame(self.root)
+        action_frame.pack(pady=10)
 
         add_product_button = tk.Button(action_frame, text="Add Product", command=self.add_product)
-        add_product_button.grid(row=0, column=0)
+        add_product_button.grid(row=0, column=0, padx=5)
 
         add_obstruction_button = tk.Button(action_frame, text="Add Obstruction", command=self.add_obstruction)
-        add_obstruction_button.grid(row=0, column=1)
+        add_obstruction_button.grid(row=0, column=1, padx=5)
 
         # Create canvas for wall representation
         self.canvas = tk.Canvas(self.root, width=800, height=600, bg="white")
