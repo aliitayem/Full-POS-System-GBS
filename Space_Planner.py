a134cd330d653c07c8918feddf2a09017eec2816
aliitayem <111657014+aliitayem@users.noreply.github.com>
2023-11-26 19:41:58 -0600

    Update Draw Slits Function

--- Space_Planner.py
+++ Space_Planner.py
@@ -1,46 +1,25 @@
 def draw_slits(self):
-    canvas_width = self.canvas.winfo_width()
-    canvas_height = self.canvas.winfo_height()
+    # Clear the canvas
+    self.canvas.delete("all")
 
-    # Clear existing slits
-    self.canvas.delete("slits")
+    # Draw the slits
+    for slit in self.slits:
+        x1, y1, x2, y2 = slit
 
-    # Get wall dimensions
-    wall_width = int(self.convert_to_inches(self.width_entry.get())) * self.conversion_rate
-    wall_height = int(self.convert_to_inches(self.height_entry.get())) * self.conversion_rate
+        # Check if the slit collides with any obstructions
+        if self.check_obstruction_collision(x1, y1, x2, y2):
+            # Split the slit into multiple segments if it collides with an obstruction
+            segments = self.split_slit(slit)
+            for segment in segments:
+                segment_x1, segment_y1, segment_x2, segment_y2 = segment
+                self.canvas.create_line(segment_x1, segment_y1, segment_x2, segment_y2, fill="red", width=2)
+        else:
+            # Draw a black line for the slit if it doesn't collide with any obstructions
+            self.canvas.create_line(x1, y1, x2, y2, fill="black", width=2)
 
-    # Calculate number of slits
-    num_slits = wall_height // (3 * self.conversion_rate)
+    # Draw the obstructions
+    for obstacle in self.obstructions:
+        x1, y1, x2, y2 = obstacle
 
-    # Define slit thickness
-    slit_thickness = int(0.5 * self.conversion_rate)
-
-    # Draw slits
-    for i in range(num_slits):
-        y = i * 3 * self.conversion_rate
-        x1 = 0
-        x2 = wall_width
-
-        while x1 < wall_width:
-            # Check for obstruction collision
-            if self.check_obstruction_collision(x1, y, x2, y):
-                x1 += self.conversion_rate
-                x2 += self.conversion_rate
-                continue
-
-            # Check for canvas boundary collision
-            if x2 > canvas_width:
-                x2 = canvas_width
-
-            # Draw slit line
-            self.canvas.create_line(x1, y, x2, y, fill="black", width=slit_thickness, tags="slits")
-
-            x1 += self.conversion_rate
-            x2 += self.conversion_rate
-
-def check_obstruction_collision(self, x1, y1, x2, y2):
-    # Check if the line collides with any obstructions
-    for obstacle in self.obstacles:
-        if x1 < obstacle[0] and x2 > obstacle[2] and y1 == obstacle[1] and y2 == obstacle[3]:
-            return True
-    return False
+        # Draw a blue rectangle for each obstruction
+        self.canvas.create_rectangle(x1, y1, x2, y2, outline="blue", width=2)
