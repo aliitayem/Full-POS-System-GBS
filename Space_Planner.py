fec46003476725d0a7c644f1135167a444765a92
aliitayem <111657014+aliitayem@users.noreply.github.com>
2023-12-10 09:45:18 -0600

    Snapping Behavior

--- Space_Planner.py
+++ Space_Planner.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import math
 
 class DragDropApp:
     def __init__(self, root):
@@ -38,6 +39,7 @@ class DragDropApp:
         self.check_collisions()
 
     def on_drag_release(self, event):
+        self.snap_to_closest_label()
         self.drag_data['widget'] = None
 
     def check_collisions(self):
@@ -66,6 +68,29 @@ class DragDropApp:
 
     def stop_movement(self):
         self.drag_data['widget'].place(x=self.drag_data['widget'].winfo_x(), y=self.drag_data['widget'].winfo_y())
+
+    def snap_to_closest_label(self):
+        closest_label = None
+        min_distance = float('inf')
+
+        for label in self.labels:
+            if label == self.drag_data['widget']:
+                continue
+
+            x1, y1, _, _ = self.get_widget_bbox(label)
+            x2, y2, _, _ = self.get_widget_bbox(self.drag_data['widget'])
+
+            dx = x1 - x2
+            dy = y1 - y2
+            distance = math.sqrt(dx**2 + dy**2)
+
+            if distance < min_distance:
+                min_distance = distance
+                closest_label = label
+
+        if closest_label:
+            x, y, _, _ = self.get_widget_bbox(closest_label)
+            self.drag_data['widget'].place(x=x, y=y)
 
 if __name__ == '__main__':
     root = tk.Tk()
